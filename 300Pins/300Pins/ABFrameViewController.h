e3e8501e5349afc8314267ad9db9f6a374e2a34b
Alex Basson <alexbasson@gmail.com>
2012-12-25 23:07:37 -0500

    Added ABFrameViewController (with XIB).

---
+++ 300Pins/300Pins/ABFrameViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABFrameViewController.h
+//  300Pins
+//
+//  Created by Alex on 12/25/12.
+//  Copyright (c) 2012 Alex Basson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABFrameViewController : UIViewController
+
+@end
---
+++ 300Pins/300Pins/ABFrameViewController.m
@@ -0,0 +1,38 @@
+//
+//  ABFrameViewController.m
+//  300Pins
+//
+//  Created by Alex on 12/25/12.
+//  Copyright (c) 2012 Alex Basson. All rights reserved.
+//
+
+#import "ABFrameViewController.h"
+
+@interface ABFrameViewController ()
+
+@end
+
+@implementation ABFrameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
