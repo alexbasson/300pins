a5a712cbf5d545f850fba1a6d01836e5a33b6b28
Alex Basson <alexbasson@gmail.com>
2012-12-26 08:27:53 -0500

    Added ABScoreView.

---
+++ 300Pins/300Pins/ABScoreView.h
@@ -0,0 +1,13 @@
+//
+//  ABScoreView.h
+//  300Pins
+//
+//  Created by Alex on 12/26/12.
+//  Copyright (c) 2012 Alex Basson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABScoreView : UIView
+
+@end
---
+++ 300Pins/300Pins/ABScoreView.m
@@ -0,0 +1,31 @@
+//
+//  ABScoreView.m
+//  300Pins
+//
+//  Created by Alex on 12/26/12.
+//  Copyright (c) 2012 Alex Basson. All rights reserved.
+//
+
+#import "ABScoreView.h"
+
+@implementation ABScoreView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
